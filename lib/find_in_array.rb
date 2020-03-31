210e3be877f04dff14f32143a1a7864145390c0e
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-31 02:05:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,12 @@
 def find_element_index(array, value_to_find)
- 
+ i = 0 
+ while i < array.length do 
+   if array == value_to_find
+     puts value_to_find[i]
+   else 
+     nil 
+   end 
+   i += 1 
 end
 
 
